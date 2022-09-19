b08eb53f8f3e9a918f1420987a6ca809483d7b26
Luiz Fernando <lfrprazeres@gmail.com>
2022-09-18 21:49:32 -0300

    4.1 - explicando o Immer

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Router from 'routes';
-import './index.css';
-import store from './store';
-import { Provider } from 'react-redux';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import Router from 'routes';
+// import './index.css';
+// import store from './store';
+// import { Provider } from 'react-redux';
+import produce from "immer"
+console.log('produce: ', produce);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Router />
-    </Provider>
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <Router />
+//     </Provider>
+//   </React.StrictMode>
+// );
