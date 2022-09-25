b2140090874b49d240677608e8b097aeebd4a553
Luiz Fernando <lfrprazeres@gmail.com>
2022-09-25 14:17:24 -0300

    5.3 - finalizando edição

--- src/store/reducers/itens.js
+++ src/store/reducers/itens.js
@@ -242,7 +242,8 @@ const itensSlice = createSlice({
       state.push({ ...payload, id: uuid() });
     },
     mudarItem: (state, { payload }) => {
-      console.log(payload);
+      const index = state.findIndex(item => item.id === payload.id);
+      Object.assign(state[index], payload.item);
     }
   }
 });
