31e5696ae83261e5bf2f83942972f0a254073664
Luiz Fernando <lfrprazeres@gmail.com>
2022-10-23 15:26:48 -0300

    2.4 - controlando o formulário

--- src/pages/Anuncie/index.js
+++ src/pages/Anuncie/index.js
@@ -2,9 +2,15 @@ import Button from 'components/Button';
 import Header from 'components/Header';
 import { useSelector } from 'react-redux';
 import styles from './Anuncie.module.scss';
+import { useForm } from 'react-hook-form';
 
 export default function Anuncie() {
   const categorias = useSelector(state => state.categorias.map(({ nome, id }) => ({ nome, id })));
+  const { register, handleSubmit } = useForm();
+
+  function cadastrar(parametro) {
+    console.log('parametro: ', parametro);
+  }
 
   return (
     <div className={styles.container}>
@@ -12,11 +18,11 @@ export default function Anuncie() {
         titulo='Anuncie aqui!'
         descricao='Anuncie seu produto no melhor site do Brasil!'
       />
-      <form className={styles.formulario}>
-        <input placeholder='Nome do produto' alt='nome do produto' />
-        <input placeholder='Descrição do produto' alt='descrição do produto' />
-        <input placeholder='URL da imagem do produto' alt='URL da imagem do produto' />
-        <select>
+      <form className={styles.formulario} onSubmit={handleSubmit(cadastrar)}>
+        <input {...register('nome')} placeholder='Nome do produto' alt='nome do produto' />
+        <input {...register('descricao')} placeholder='Descrição do produto' alt='descrição do produto' />
+        <input {...register('imagem')} placeholder='URL da imagem do produto' alt='URL da imagem do produto' />
+        <select {...register('categoria')}>
           <option value='' disabled > Selecione a categoria </option>
           {categorias.map(categoria => (
             <option key={categoria.id} value={categoria.id}>
@@ -24,7 +30,7 @@ export default function Anuncie() {
             </option>
           ))}
         </select>
-        <input type='number' placeholder='Preço do produto' />
+        <input {...register('preco')} type='number' placeholder='Preço do produto' />
         <Button type='submit'>
           Cadastrar produto
         </Button>
